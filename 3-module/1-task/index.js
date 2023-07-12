17b7a775fa9826c6513a12e73984ed237c586b8e
Roman Andrianov <r_andrianov02@mail.ru>
2023-07-12 11:28:01 +0300

    Выполнение всех заданий с Занятия 3

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,4 @@
 function namify(users) {
-  // ваш код...
+  let tmp = users.map((user) => user.name)
+  return tmp;
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
 function getMinMax(str) {
-  // ваш код...
+  return str
+    .split(' ')
+    .filter(value => isFinite(value))
+    .map(value => parseFloat(value))
+    .reduce((number, item) => {
+        if (item > number.max) {
+          number.max = item;
+        }
+        if (item < number.min) {
+          number.min = item;
+        }
+        return number;
+      },
+      { min: Infinity, max: -Infinity }
+    );
 }
