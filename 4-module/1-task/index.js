618b8fb5897ada20c76d315fa1d734442cc4b0e2
Roman Andrianov <r_andrianov02@mail.ru>
2023-07-16 20:07:51 +0300

    Выполнение всех заданий с Занятия 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
 function makeFriendsList(friends) {
-  // ваш код...
+    
+  let ul = document.createElement('ul');
+  document.body.append(ul);
+
+  for(let value of friends){
+      let li = document.createElement('li');
+      li.innerHTML = `${value.firstName} ${value.lastName}`;
+      ul.append(li);
+  }
+
+  return ul;
+
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+
+    let row = table.rows[i];
+    console.log(row);
+    console.log(row.cells[3]);
+    
+    if(row.cells[3].hasAttribute('data-available')){
+        if(row.cells[3].getAttribute('data-available') ===  'true'){
+            row.classList.add('available');
+            console.log('true');
+        } else {
+            row.classList.add('unavailable');
+        }
+    } else {
+        row.hidden = true; 
+    }
+
+    if(row.cells[2].innerHTML == 'm'){
+        row.classList.add('male');
+    } else {
+        row.classList.add('female');
+    }
+
+    if(row.cells[1].innerHTML < 18){
+        row.style.textDecoration = "line-through";
+    }
+  }
 }
