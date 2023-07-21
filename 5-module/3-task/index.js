4874d3e859a9b197e4791af0ebc6ec65783b4cc0
Roman Andrianov <r_andrianov02@mail.ru>
2023-07-21 13:21:30 +0300

    Выполнение всех заданий с Занятия 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+
+  //buttons
+  let leftArrow = document.querySelector('.carousel__arrow_left');
+  let rightArrow = document.querySelector('.carousel__arrow_right');
+
+  leftArrow.style.display = 'none';
+
+  //img
+  let step = 0;
+  let caruselInner = document.querySelector('.carousel__inner');
+  let img = document.querySelectorAll('.carousel__img');
+
+  let size = img[0].offsetWidth;
+
+  
+  rightArrow.addEventListener('click', () => {
+    leftArrow.style.display = '';
+    step++;
+    caruselInner.style.transform = 'translateX(' + `${-size * step}px)`;
+    if(step == img.length - 1){
+      rightArrow.style.display = 'none';
+    }
+
+  })
+  leftArrow.addEventListener('click', () => {
+    rightArrow.style.display = '';
+    step--;
+    caruselInner.style.transform = 'translateX(' + `${-size * step}px)`;
+    if(step == 0){
+      leftArrow.style.display = 'none';
+    }
+
+  })
 }
