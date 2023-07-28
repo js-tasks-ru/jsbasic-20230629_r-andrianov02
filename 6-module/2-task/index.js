cd219bc52618ba84c058073c1dfaa1afafb30545
Roman Andrianov <r_andrianov02@mail.ru>
2023-07-28 12:07:31 +0300

    Выполнение всех заданий с Занятия 6

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,40 @@
 export default class ProductCard {
+  elem = null;
+
   constructor(product) {
+    this.product = product;
+    this.elem = document.createElement("div");
+    this.template();
+  }
+  get elem(){
+    return this.elem;
+  }
+
+  template(){
+    let div = `
+      <div class="card">
+      <div class="card__top">
+          <img src="/assets/images/products/${this.product.image}" class="card__image" alt="product">
+          <span class="card__price">€${this.product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+          <div class="card__title">${this.product.name}</div>
+          <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>
+      </div>
+    `;
+    this.elem.innerHTML = div;
+    for(let btn of this.elem.querySelectorAll('.card__button')){
+      btn.addEventListener("click", this.render);
+  }}
+
+  render = () => {
+    let event = new CustomEvent("product-add", {
+      detail: this.product.id,
+      bubbles: true,
+  });
+    this.elem.dispatchEvent(event);
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,109 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
+  elem = null;
+
   constructor(slides) {
     this.slides = slides;
+
+    this.elem = this.render();
+  }
+
+  get elem() {
+    return this._container;
+  }
+
+  template(){ 
+    return `
+    <div class="carousel">
+    <div class="carousel__arrow carousel__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </div>
+    <div class="carousel__arrow carousel__arrow_left">
+      <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+    </div>
+    <div class="carousel__inner">` + this.slides.map(value => `
+    <div class="carousel__slide" data-id="${value.id}">
+    <img src="/assets/images/carousel/${value.image}" class="carousel__img" alt="slide">
+    <div class="carousel__caption">
+      <span class="carousel__price">€${value.price.toFixed(2)}</span>
+      <div class="carousel__title">${value.name}</div>
+      <button type="button" class="carousel__button">
+        <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+      </button>
+    </div>
+    </div>
+  `).join("");
+}
+
+  render(){
+    this.elem = createElement(this.template());
+
+    this.initCarousel();
+
+    for(let btn of this.elem.querySelectorAll('.carousel__button')){
+      btn.addEventListener("click", this.onBtnClick);
+    }
+
+    return this.elem;
+  }
+
+  onBtnClick = ({target}) => {
+
+    let slide = target.closest('.carousel__slide').dataset;
+
+    let event = new CustomEvent("product-add", {
+      detail: slide.id,
+      bubbles: true,
+    });
+    console.log(event.detail);
+    this.elem.dispatchEvent(event);
+  }
+
+  initCarousel() {
+
+    //buttons
+    let leftArrow = this.elem.querySelector('.carousel__arrow_left');
+    let rightArrow = this.elem.querySelector('.carousel__arrow_right');
+
+    leftArrow.style.display = 'none';
+  
+    //img
+    let step = 0;
+
+    // Перенес во внуть события(1, 2)
+    // let caruselInner = this.elem.querySelector('.carousel__inner');
+    // let img = this.elem.querySelectorAll('.carousel__img');
+    // let size = img[0].offsetWidth;
+    // console.log(size);
+
+    rightArrow.addEventListener('click', () => {
+      leftArrow.style.display = '';
+      step++;
+      // 1
+      let caruselInner = this.elem.querySelector('.carousel__inner');
+      let img = this.elem.querySelectorAll('.carousel__img');
+      let size = img[0].offsetWidth;
+
+      caruselInner.style.transform = 'translateX(' + `${-size * step}px)`;
+      if(step == img.length - 1){
+        rightArrow.style.display = 'none';
+      }
+  
+    })
+    leftArrow.addEventListener('click', () => {
+      rightArrow.style.display = '';
+      step--;
+      // 2
+      let caruselInner = this.elem.querySelector('.carousel__inner');
+      let img = this.elem.querySelectorAll('.carousel__img');
+      let size = img[0].offsetWidth;
+
+      caruselInner.style.transform = 'translateX(' + `${-size * step}px)`;
+      if(step == 0){
+        leftArrow.style.display = 'none';
+      }
+  
+    })
   }
 }
